faa7f19d7afbce1b988b04e3ccfa83c0168dfa3b
AnaGmzPstr <ana.gomez.7e6@itb.cat>
2025-02-11 14:36:44 +0100

    commit

--- pinia-course/src/App.vue
+++ pinia-course/src/App.vue
@@ -5,8 +5,8 @@ import ProductCard from "@/components/ProductCard.vue";
 
 //import products from "@/data/products.json";
 
-import {useProductStore} from "./stores/ProductStores.js";
-import {useCartStore} from "./stores/CartStore.js";
+import { useProductStore } from "./stores/ProductStores.js";
+import { useCartStore } from "./stores/CartStore.js";
 
 import { storeToRefs } from "pinia";
 
@@ -25,13 +25,39 @@ productStore.fill()
 
 // ho podem agrupar amb
 const addToCart = (count, product) => {
-  count=parseInt(count)
-  cartStore.$patch(state=>{
+  count = parseInt(count)
+  cartStore.$patch(state => {
     for (let index = 0; index < count; index++) {
       state.items.push(product);
     }
   })
-} 
+}
+
+//rebrà una callback function que rebrà unes opcions
+//name: nom de l'acció
+//store instace en el nostre cas cartStore
+//args : arguments passats a l'action que son count i product
+//també afegim un after hook que permet executar qualsevol
+//Cosa després que l'acció retorni i resolgui,
+//onError hook per saber quan passa una excepció o falla i fer nosaltres alguna acció
+cartStore.$onAction(({
+  name,
+  store,
+  args,
+  after,
+  onError
+}) => {
+  if (name === 'addItems') {
+    after(() => {
+      console.log(args[0])
+    });
+    onError((error) => {
+      console.log("Hello error:", error.message);
+    });
+
+  }
+})
+
 
 
 
@@ -42,12 +68,8 @@ const addToCart = (count, product) => {
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard
-        v-for="product in productStore.products"
-        :key="product.name"
-        :product="product"
-        @addToCart="cartStore.addItems($event, product)"
-      />
+      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
+        @addToCart="cartStore.addItems($event, product)" />
     </ul>
   </div>
 </template>
